75bbab12c347f98252ec38912000afb81ce2ec42
Alex Holmes <alex@sed.dev>
2025-09-30 21:08:24 -0400

    Move helper test func and flag

--- pkg/db_test.go
+++ pkg/db_test.go
@@ -1,12 +1,34 @@
 package pkg
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"sync"
 	"testing"
 	"time"
 )
+
+// Use -slow flag to run longer tests
+var slow = flag.Bool("slow", false, "run slow tests")
+
+// Helper to create a temporary test database
+func setupTestDB(t *testing.T) *db {
+	tmpfile := fmt.Sprintf("/tmp/test_btree_%s.db", t.Name())
+	_ = os.Remove(tmpfile)
+
+	db, err := Open(tmpfile)
+	if err != nil {
+		t.Fatalf("Failed to create DB: %v", err)
+	}
+
+	t.Cleanup(func() {
+		_ = db.Close()
+		_ = os.Remove(tmpfile)
+	})
+
+	return db
+}
 
 func TestDBBasicOperations(t *testing.T) {
 	t.Parallel()
